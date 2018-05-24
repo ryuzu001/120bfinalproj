10f7181e3984372565d811f06f0e47eca7b83076
Ryan Yuzuki <ryuzu001@ucr.edu>
2018-05-24 11:42:35 -0700

    custom char

--- GccApplication10/main.c
+++ GccApplication10/main.c
@@ -16,37 +16,123 @@
 char hello[50] = "Hello World abcdefghijklmnop";
 char world[50] = "World!";
 
+void displayMenu();
+
 void LCD_Custom_Char (unsigned char loc, unsigned char *msg)
 {
 	int i;
-	LCD_WriteCommand (0x40 + (loc*8));	/* Command 0x40 and onwards forces the device to point CGRAM address */
-	for(i=0;i<7;i++){	/* Write 8 byte for generation of 1 character */
+	LCD_WriteCommand (0x40 + (loc*8));	/* Command 0x40 for CGRAM */
+	for(i=0;i<7;i++){	/* 7 cause the bottom is reserved for the cursor */
 		LCD_WriteData(msg[i]);
 	}
 	LCD_WriteCommand(0x80);
 }
 
+void displayMenu(){
+	// select difficulty
+	// set time
+	// play game
+	// something else here
+	
+}
+
 int main() {
-	DDRA = 0xFF; PORTA = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
 	DDRD = 0xFF; PORTD = 0x00; // LCD control lines
 	
 	// Initializes the LCD display
-	//unsigned char Character1[8] = { 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0x00 }; 
-	// unsigned char Character1;
-	unsigned char battery[7] = {0x0e, 0x0a, 0x1b, 0x11, 0x11, 0x11, 0x1f};
+	//unsigned char block1[7] = { 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f, 0x1f}; 
+		
+	unsigned char crosshatch[7] = { 0x15, 0x0a, 0x15, 0x0a, 0x15, 0x0a, 0x15}; 	
+	unsigned char cross[7] = {0x00, 0x0e, 0x1b, 0x11, 0x1b, 0x0e, 0x00 };
+	unsigned char smiley[7] = {0x00, 0x11, 0x00, 0x04, 0x11, 0x0e, 0x00};	
+		
+	unsigned char selH[7] = {0x1f, 0x15, 0x15, 0x11, 0x15, 0x15, 0x1f};	
+	unsigned char selE[7] = {0x1f, 0x11, 0x17, 0x13, 0x17, 0x11, 0x1f};
+	unsigned char selL[7] = {0x1f, 0x17, 0x17, 0x17, 0x17, 0x11, 0x1f};
+	unsigned char selO[7] = {0x1f, 0x11, 0x15, 0x15, 0x15, 0x11, 0x1f};
+		
 	/* Custom char set for alphanumeric LCD Module */
 	
 	
 	
 	LCD_init();
 	
-	LCD_WriteCommand(0x00);
-	LCD_Custom_Char(0, battery);  /* Build Character1 at position 0 */
-	LCD_WriteData(0);
+	/*
+	LCD_WriteCommand(0x01);
+	LCD_Custom_Char(1, battery);
+	LCD_WriteData(1);*/
 	
-	//LCD_ClearScreen();
+	
 	// LCD_DisplayString(1, hello);
-	while(1);
+	
+	LCD_WriteCommand(0x38); /* function set */
+	LCD_WriteCommand(0x0c); /* display on,cursor off,blink off */
+	TimerSet(1000);
+	TimerOn(); 
+	
+	LCD_ClearScreen();
+	while(1){	
+		unsigned char cur= 1;
+		LCD_Custom_Char(0, crosshatch);  // Build cross at position 0 
+		LCD_Custom_Char(1, cross);
+		LCD_Custom_Char(2, smiley);
+		LCD_Custom_Char(3, selH);
+		LCD_Custom_Char(4, selE);
+		LCD_Custom_Char(5, selL);
+		LCD_Custom_Char(6, selO);
+		
+		LCD_Cursor(cur++);
+		LCD_WriteData(0);			  // Writes position 0 
+		LCD_Cursor(cur++);
+		LCD_WriteData(' ');
+		LCD_Cursor(cur++);
+		LCD_WriteData(1);
+		LCD_Cursor(cur++);
+		LCD_WriteData(' ');
+		LCD_Cursor(cur++);
+		LCD_WriteData(2);
+		LCD_Cursor(cur++);
+		LCD_WriteData(' ');
+		LCD_Cursor(cur++);
+		LCD_WriteData('H');
+		LCD_Cursor(cur++);
+		LCD_WriteData('E');
+		LCD_Cursor(cur++);
+		LCD_WriteData('L');
+		LCD_Cursor(cur++);
+		LCD_WriteData('L');
+		LCD_Cursor(cur++);
+		LCD_WriteData('O');
+		LCD_Cursor(cur++);
+		LCD_WriteData(' ');
+		LCD_Cursor(cur++);
+		LCD_WriteData(3);
+		LCD_Cursor(cur++);
+		LCD_WriteData(4);
+		LCD_Cursor(cur++);
+		LCD_WriteData(5);
+		LCD_Cursor(cur++);
+		LCD_WriteData(5);
+		LCD_Cursor(cur++);
+		LCD_WriteData(6);
+		LCD_Cursor(cur++);
+		LCD_WriteData(' ');
+		LCD_Cursor(cur++);
+		LCD_WriteData(3);
+		LCD_Cursor(cur++);
+		LCD_WriteData(4);
+		LCD_Cursor(cur++);
+		LCD_WriteData(5);
+		LCD_Cursor(cur++);
+		LCD_WriteData(5);
+		LCD_Cursor(cur++);
+		LCD_WriteData(6);	
+		
+		
+		while(!TimerFlag);
+		TimerFlag = 0;
+	}
 }
 
 
